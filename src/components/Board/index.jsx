82bad64bf8da66219b2d0c16ec788a10a3591df2
alexx <alexxc46@gmail.com>
2023-06-08 17:57:18 +0300

    taken isHidden prop to a cell

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -4,17 +4,14 @@ import './index.css';
 
 const Board = () => {
   const rows = 10;
-  const cols = 10;
-
-  const hiddenCells = [[4, 2], [4, 3], [5, 2], [5, 3], [4, 6], [4, 7], [5, 6], [5, 7]]
-  const isHidden = (x, y) => hiddenCells.find(cell => cell[0] === x && cell[1] === y);
+  const cols = 10; 
 
   return (
     <div className="board">
       {Array.from({ length: rows }).map((_, rowIndex) => (
         <div className="row" key={rowIndex}>
           {Array.from({ length: cols }).map((_, colIndex) => (
-            <Cell key={`${colIndex}-${rowIndex}`} x={rowIndex} y={colIndex} isHidden={isHidden(rowIndex, colIndex)} />
+            <Cell key={`${rowIndex}-${colIndex}`} x={rowIndex} y={colIndex} />
           ))}
         </div>
       ))}
--- src/components/Cell/index.jsx
+++ src/components/Cell/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './index.css';
 
-const Cell = ({ x, y, isHidden }) => { 
+const Cell = ({ x, y }) => { 
+  const hiddenCells = [[4, 2], [4, 3], [5, 2], [5, 3], [4, 6], [4, 7], [5, 6], [5, 7]]
+  const isHidden = hiddenCells.find(cell => cell[0] === x && cell[1] === y);
 
   return (
-    <div className={`${isHidden && 'not-active'} cell`}>
+    <div className='cell'>
+      <div className={`${isHidden && 'not-active'}`}> 
       ({x}, {y})
+      </div>
     </div>
   );
 };

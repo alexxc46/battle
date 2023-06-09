c7e9c12f2ac6f61bd7e267b0f08f89fa7e605462
alexx <alexxc46@gmail.com>
2023-06-09 13:04:24 +0300

    active cell highlighted

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Cell from '../Cell';
 import './index.css';
 
@@ -6,15 +6,17 @@ const Board = () => {
   const rows = 10;
   const cols = 10; 
 
-  const [clickedCell, setClickedCell] = useState(null);
+  const [clickedCell, setClickedCell] = useState([]);
 
-  const handleClick = (x, y) => {
+  // reference to this function is passed to a component and called from there
+  const handleClick = (x, y, isHidden) => {
     console.log('Cell clicked:', x, y);
+    !!isHidden && console.log('The cell IS HIDDEN')
     setClickedCell({ x, y });
-  };
+  }
 
   return (
-    <div className="board" onClick={() => setClickedCell(null)}>
+    <div className="board">
       {Array.from({ length: rows }).map((_, rowIndex) => (
         <div className="row" key={rowIndex}>
           {Array.from({ length: cols }).map((_, colIndex) => (
@@ -23,12 +25,13 @@ const Board = () => {
               x={rowIndex}
               y={colIndex}
               onClick={handleClick}
+              clickedCell={clickedCell}
             />
           ))}
         </div>
       ))}
     </div>
   );
-};
+}
 
 export default Board;
--- src/components/Cell/index.jsx
+++ src/components/Cell/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css';
 
-const Cell = ({ onClick, x, y }) => {
+const Cell = ({ onClick, x, y, clickedCell }) => {
   const hiddenCells = [[4, 2], [4, 3], [5, 2], [5, 3], [4, 6], [4, 7], [5, 6], [5, 7]]
   const isHidden = hiddenCells.find(cell => cell[0] === x && cell[1] === y);
-
+  const isActive = x === clickedCell?.x && y === clickedCell?.y
   return (
-    <div className="cell"  onClick={() => onClick(x, y)}>
-      <div className={`${isHidden && 'not-active'}`}> 
+    <div className={`cell ${isActive && 'is-active'}`}  onClick={() => onClick(x, y, isHidden)}>
+      <div className={`${isHidden && 'is-hidden'}`}> 
       [{x}, {y}]
       </div>
     </div>

298198802e554ed5c5396423fb7ff80a4e6e93b2
alexx <alexxc46@gmail.com>
2023-06-09 11:57:52 +0300

    temp

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Cell from '../Cell';
 import './index.css';
 
@@ -6,8 +6,20 @@ const Board = () => {
   const rows = 10;
   const cols = 10; 
 
+  const [clickedCell, setClickedCell] = useState(null);
+
+  // useEffect(() => {
+  //   console.log(clickedCell)
+  // }, [clickedCell])
+
+  const handleClick = (x, y, evt) => {
+    console.log('event happened')
+    // event.stopPropagation(); // Stop event propagation to the board
+    // setClickedCell({ x, y });
+  };
+
   return (
-    <div className="board">
+    <div className="board" onClick={() => console.log('click on the board fierfd')}>
       {Array.from({ length: rows }).map((_, rowIndex) => (
         <div className="row" key={rowIndex}>
           {Array.from({ length: cols }).map((_, colIndex) => (
--- src/components/Cell/index.jsx
+++ src/components/Cell/index.jsx
@@ -4,11 +4,14 @@ import './index.css';
 const Cell = ({ x, y }) => { 
   const hiddenCells = [[4, 2], [4, 3], [5, 2], [5, 3], [4, 6], [4, 7], [5, 6], [5, 7]]
   const isHidden = hiddenCells.find(cell => cell[0] === x && cell[1] === y);
-
+  const handleClick = (e) => {
+    console.log('lcick handleds')
+    e.stopPropagation()
+  }
   return (
-    <div className='cell'>
+    <div onClick={handleClick} className='cell'>
       <div className={`${isHidden && 'not-active'}`}> 
-      ({x}, {y})
+      [{x}, {y}]
       </div>
     </div>
   );
